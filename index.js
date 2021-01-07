42ad6747ab920537241b90a7a75c415f37be87b4
lutungp <lntngp19@gmail.com>
2021-01-07 20:16:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// File : index.js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./config/db");
+
+const user = require("./routes/user");
+
+InitiateMongoServer();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
